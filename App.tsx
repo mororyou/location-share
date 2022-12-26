bcaa4523af5cb01935625e40c4797c679cc0153e
tomare-moroizumi <m.ryousuke0401@icloud.com>
2022-12-27 01:14:33 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { LogBox } from 'react-native';
+import { Provider } from 'react-redux';
+import { RootNavigator } from './navigations/RootNavigator';
+import { store } from './store';
 
 export default function App() {
+  LogBox.ignoreLogs([
+    'Setting a timer',
+    'AsyncStorage has been extracted from react-native core and will be removed in a future release',
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
